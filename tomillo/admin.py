54d4780ba89b85e8d6aa7fc4522dd4caaa4a7a27
Santiago Fuentes <santiagofp9@gmail.com>
2020-04-07 11:51:02 +0200

    Modelo de datos
    Listo el archivo en fichero, listo en Atlas

--- tomillo/admin.py
+++ tomillo/admin.py
@@ -3,6 +3,12 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Producto)
-admin.site.register(Carrito)
-admin.site.register(Categoria)
+admin.site.register(Numero)
+admin.site.register(Aliado)
+admin.site.register(Programa)
+admin.site.register(Promocion)
+admin.site.register(Alumni)
+admin.site.register(Contacto)
+admin.site.register(Prensa)
+admin.site.register(Publicacion)
+admin.site.register(Recurso)
