5397f3c2fed535b108058fdcb557a10a7186a872
Aliona <alyona.saenco@gmail.com>
2020-04-16 09:56:59 +0200

    barra de navegación
    la web adaptada al árbol de contenido

--- tomillo/urls.py
+++ tomillo/urls.py
@@ -4,8 +4,17 @@ from .views import *
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('',Inicio.as_view(), name = 'inicio'),
-    path('single/',Single.as_view(), name = 'single'),
-    path('blog/',Blog.as_view(), name = 'blog'),
-    path('contact/',Contact.as_view(), name = 'contact'),
+    path('', Inicio.as_view(), name = 'inicio'),
+    path('about_us/', AboutUs.as_view(), name = 'about_us'),
+    path('partners/', Partners.as_view(), name = 'partners'),
+    path('blog/', Blog.as_view(), name = 'blog'),
+    path('contact/', Contact.as_view(), name = 'contact'),
+    path('f5/', TomilloF5.as_view(), name = 'f5'),
+    path('cybersecurity/', Cybersecurity.as_view(), name = 'cybersecurity'),
+    path('formal_education/', FormalEducation.as_view(), name = 'formal_education'),
+    path('non_formal_education/', NonFormalEducation.as_view(), name = 'non_formal_education'),
+    path('resources/', Resources.as_view(), name = 'resources'),
+    path('alumni/', Alumnis.as_view(), name = 'alumni'),
+    path('press/', Press.as_view(), name = 'press'),
+    path('legal/', Legal.as_view(), name = 'legal'),
   ]
--- tomillo/views.py
+++ tomillo/views.py
@@ -17,13 +17,40 @@ from django.views.generic.edit import CreateView,FormView
 class Inicio(TemplateView):
     template_name = 'index.html'
 
-class Single(TemplateView):
-    template_name = 'tomillo/single.html'
+class AboutUs(TemplateView):
+    template_name = 'tomillo/aboutUs.html'
+
+class Partners(TemplateView):
+    template_name = 'tomillo/partners.html'
 
 class Blog(TemplateView):
     template_name = 'tomillo/archive.html'
 
 class Contact(TemplateView):
     template_name = 'tomillo/contact.html'
+
+class TomilloF5(TemplateView):
+    template_name = 'tomillo/f5.html'
+
+class Cybersecurity(TemplateView):
+    template_name = 'tomillo/cyber.html'
+
+class FormalEducation(TemplateView):
+    template_name = 'tomillo/formalEducation.html'
+
+class NonFormalEducation(TemplateView):
+    template_name = 'tomillo/nonformalEducation.html'
+
+class Resources(TemplateView):
+    template_name = 'tomillo/resources.html'
+    
+class Alumnis(TemplateView):
+    template_name = 'tomillo/alumni.html'
+
+class Press(TemplateView):
+    template_name = 'tomillo/press.html'
+
+class Legal(TemplateView):
+    template_name = 'tomillo/legal.html'
 
 
