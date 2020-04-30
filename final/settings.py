ec10055f6b3bec079993c0f7d16564027067e240
k4renzit@ <karen_t195@hotmail.com>
2020-04-30 10:27:08 +0200

    Prensa
    Se agregó "botón siguiente"

--- final/settings.py
+++ final/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@4b=tk_rryd6insle6peg(&%g(je6lx*hnv87v)%tn0!c6mmid'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['gotech.alwaysdata.net']
+ALLOWED_HOSTS = ['gotech.alwaysdata.net','127.0.0.1']
 
 
 # Application definition
